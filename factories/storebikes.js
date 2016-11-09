75d6ddb74c8562668a46fa5750b3b326f443aaa1
[John Patterson] <pattersonjc1@gmail.com>
2016-11-09 11:38:36 -0500

    fixed  being correctly passed through to getWeather function in the factory

--- factories/storebikes.js
+++ factories/storebikes.js
@@ -4,10 +4,10 @@
       .factory('storebikes', function($http) {
          var bikes = [];
 
-         const getWeather = function() {
+         const getWeather = function(city) {
             var call = $http({
                method: 'GET',
-               url: 'http://api.openweathermap.org/data/2.5/weather?q=Cincinnati&APPID=7cf16558d759d14815306832bd7341e2&units=imperial'
+               url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7cf16558d759d14815306832bd7341e2&units=imperial`
 
             });
             return call;
